d65748f9e0ddab57ef1e47f3eaaf637d4d6834b7
Jake Paine <jake.paine@me.com>
2011-06-21 10:44:28 +0100

    Added dll imports and strings.

--- ProxySwitch/ProxySwitch/MainForm.cs
+++ ProxySwitch/ProxySwitch/MainForm.cs
@@ -6,11 +6,24 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml;
+using System.Runtime.InteropServices;
 
 namespace ProxySwitch
 {
     public partial class MainForm : Form
     {
+        // Import wininet.dll to allow us to refresh Internet Settings
+        // without exiting the web browser.
+        [DllImport("wininet.dll")]
+        public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int dwBufferLength);
+        public const int INTERNET_OPTION_SETTINGS_CHANGED = 39;
+        public const int INTERNET_OPTION_REFRESH = 37;
+
+        string proxyAddress;
+        string proxyPort;
+        string proxyException;
+
         public MainForm()
         {
             InitializeComponent();
